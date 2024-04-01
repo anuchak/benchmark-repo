484e164ac1162e3cfaaa78dbe5281fc7a9d71078
a8chakra <anurag.chakraborty98@gmail.com>
2024-04-01 13:16:40 -0400

    push simd code

--- simd_stuff/main2.cpp
+++ simd_stuff/main2.cpp
@@ -1,4 +1,4 @@
-#include "emmintrin.h"
+#include "immintrin.h"
 #include <chrono>
 #include <iostream>
 #include <math.h>
@@ -9,13 +9,13 @@ void normal(float *a, int N) {
     a[i] = sqrt(a[i]);
 }
 
-void sse(float *a, int N) {
+void avx(float *a, int N) {
   // We assume N % 4 == 0.
-  int nb_iters = N / 4;
-  __m128 *ptr = (__m128 *)a;
+  int nb_iters = N / 8;
+  __m256 *ptr = (__m256 *)a;
 
-  for (int i = 0; i < nb_iters; ++i, ++ptr, a += 4)
-    _mm_store_ps(a, _mm_sqrt_ps(*ptr));
+  for (int i = 0; i < nb_iters; ++i, ++ptr, a += 8)
+    _mm256_store_ps(a, _mm256_sqrt_ps(*ptr));
 }
 
 int main(int argc, char **argv) {
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
   int N = atoi(argv[1]);
 
   float *a;
-  posix_memalign((void **)&a, 16, N * sizeof(float));
+  posix_memalign((void **)&a, 32, N * sizeof(float));
 
   for (int i = 0; i < N; ++i)
     a[i] = 3141592.65358;
@@ -36,15 +36,15 @@ int main(int argc, char **argv) {
   auto duration1 = std::chrono::system_clock::now().time_since_epoch();
   auto millis1 =
       std::chrono::duration_cast<std::chrono::nanoseconds>(duration1).count();
-  printf("%lu nanoseconds\n", millis1 - millis);
+  printf("%lu nanosec | ", millis1 - millis);
   for (int i = 0; i < N; ++i)
     a[i] = 3141592.65358;
   duration = std::chrono::system_clock::now().time_since_epoch();
   millis =
       std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
-  sse(a, N);
+  avx(a, N);
   duration1 = std::chrono::system_clock::now().time_since_epoch();
   millis1 =
       std::chrono::duration_cast<std::chrono::nanoseconds>(duration1).count();
-  printf("%lu nanoseconds\n", millis1 - millis);
+  printf("%lu nanosec\n", millis1 - millis);
 }
